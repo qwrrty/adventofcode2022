4b282a8dc4aa069a7d98332df48af33e179a8f23
Tim Pierce <twp@unchi.org>
2022-12-03 14:02:50 -0500

    moving day 1 solution to its own class

---
+++ Advent of Code 2022/Day01.cs
@@ -0,0 +1,49 @@
+﻿using System;
+namespace Advent_of_Code_2022
+{
+    class Elf
+    {
+        public List<int> Foods { get; set; }
+
+        public Elf() { Foods = new List<int>(); }
+
+        public Elf(List<int> data) { Foods = data; }
+
+        public int TotalCalories()
+        {
+            return Foods.Sum();
+        }
+    }
+
+    public class Day01
+	{
+        public Day01()
+        {
+            String inputFileName = "/Users/twp/Downloads/day_01.txt";
+
+            List<Elf> elves = new();
+            List<int> foods = new();
+
+            foreach (String line in System.IO.File.ReadLines(inputFileName))
+            {
+                if (line.Length == 0)
+                {
+                    elves.Add(new Elf(foods));
+                    foods = new();
+                    continue;
+                }
+                foods.Add(int.Parse(line));
+            }
+
+            // the elf with the most calories:
+            Console.WriteLine(elves.OrderByDescending(e => e.TotalCalories())
+                .First()
+                .TotalCalories());
+
+            // the three elves with the most calories:
+            Console.WriteLine(elves.OrderByDescending(e => e.TotalCalories())
+                .Take(3)
+                .Sum(e => e.TotalCalories()));
+        }
+    }
+}
