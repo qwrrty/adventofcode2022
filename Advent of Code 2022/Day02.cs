123c8aa1cbc813ca1fdb3b69b2a70e3e69b6dabe
Tim Pierce <twp@unchi.org>
2022-12-04 14:32:46 -0500

    Added Day 2 part 2

--- Advent of Code 2022/Day02.cs
+++ Advent of Code 2022/Day02.cs
@@ -1,4 +1,9 @@
-﻿using System;
+﻿// This is a mess.
+// For another problem, I'd try a little harder to find an elegant or
+// more readable solution, but as it is this feels ridiculously
+// overengineered, so I'm calling it a day and moving on.
+
+using System;
 namespace Advent_of_Code_2022
 {
     public class Day02
@@ -33,11 +38,47 @@ namespace Advent_of_Code_2022
             return shapeValues[v[1]] + scoreTable[line];
         }
 
+        public static int Score_v2(string line)
+        {
+            // Score a play according to Day 2 part 2
+            // (X means a loss, Y means a draw, Z means a win)
+
+            var shapeValues = new Dictionary<string, int>
+            {
+                { "A", 1 },
+                { "B", 2 },
+                { "C", 3 }
+            };
+            var outcomeScores = new Dictionary<string, int>
+            {
+                { "X", 0 },
+                { "Y", 3 },
+                { "Z", 6 }
+            };
+            var rankedShapes = new string[] { "A", "B", "C" };
+
+            var v = line.Split(' ');
+            string shape;
+
+            var i = shapeValues[v[0]] - 1;
+
+            // choose a counter play based on whether we need to lose
+            // (X), draw (Y), or win (Z)
+            switch (v[1])
+            {
+                case "X": shape = rankedShapes[(i + 2) % 3]; break;
+                case "Z": shape = rankedShapes[(i + 1) % 3]; break;
+                default: shape = v[0]; break;
+            }
+
+            return shapeValues[shape] + outcomeScores[v[1]];
+        }
+
         public Day02()
         {
             foreach (String line in System.IO.File.ReadLines(inputFileName))
             {
-                totalScore += score(line);
+                totalScore += Score_v2(line);
             }
         }
 
